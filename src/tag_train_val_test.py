7b536d2208b2bbcf78217c09d7b5d8d69bc8b170
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-11-08 19:16:33 +0200

    implementation

--- src/tag_train_val_test.py
+++ src/tag_train_val_test.py
@@ -1,6 +1,6 @@
 import os
 import random
-import string
+from collections import defaultdict
 import supervisely_lib as sly
 
 TEAM_ID = int(os.environ['context.teamId'])
@@ -11,25 +11,66 @@ my_app = sly.AppService()
 PROJECT = None
 TOTAL_IMAGES_COUNT = None
 
+
+def sample_images(api, datasets, train_images_count):
+    all_images = []
+    for dataset in datasets:
+        images = api.image.get_list(dataset.id)
+        all_images.extend(images)
+    cnt_images = len(all_images)
+
+    shuffled_images = all_images.copy()
+    random.shuffle(shuffled_images)
+
+    train_images = shuffled_images[:train_images_count]
+    val_images = shuffled_images[train_images_count:]
+
+    ds_images_train = defaultdict(list)
+    for image_info in train_images:
+        ds_images_train[image_info.dataset_id].append(image_info)
+
+    ds_images_val = defaultdict(list)
+    for image_info in val_images:
+        ds_images_val[image_info.dataset_id].append(image_info)
+
+    return ds_images_train, ds_images_val, len(train_images), len(val_images)
+
 @my_app.callback("assign_tags")
 @sly.timeit
 def assign_tags(api: sly.Api, task_id, context, state, app_logger):
-    train_count = state["count.train"]
-    val_count = state["count.val"]
+    train_count = state["count"]["train"]
+    val_count = state["count"]["val"]
     share_images = state["shareImagesBetweenSplits"]
+
+    inplace = state["inplace"]
+    if inplace is True:
+        raise NotImplementedError("Inplace operation will be implemented in the future...")
+
+    datasets = api.dataset.get_list(PROJECT.id)
+    images_train, images_val, _cnt_train, _cnt_val = sample_images(api, datasets, train_count)
+
     if share_images is True:
         if train_count != val_count:
             raise ValueError("Share images option is enabled, but train_count != val_count")
+        if _cnt_val == 0:
+            images_val = images_train
+        else:
+            raise RuntimeError("_cnt_val != 0")
     else:
         if train_count + val_count != TOTAL_IMAGES_COUNT:
             raise ValueError("train_count + val_count != TOTAL_IMAGES_COUNT")
+
+
+
+
+
 
 
 def main():
     sly.logger.info("Input params", extra={"context.teamId": TEAM_ID,
                                            "context.workspaceId": WORKSPACE_ID,
                                            "modal.state.slyProjectId": PROJECT_ID})
-
+    global PROJECT, TOTAL_IMAGES_COUNT
 
     api = sly.Api.from_env()
     PROJECT = api.project.get_info_by_id(PROJECT_ID)
@@ -67,7 +108,8 @@ def main():
             "val": 100 - train_percent
         },
         "shareImagesBetweenSplits": False,
-        "sliderDisabled": False
+        "sliderDisabled": False,
+        "inplace": False
     }
 
     # Run application service
