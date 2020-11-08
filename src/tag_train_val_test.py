19fcd34931aa001a438e0bd5b357366a12fe0461
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-11-08 18:51:13 +0200

    start implementation

--- src/tag_train_val_test.py
+++ src/tag_train_val_test.py
@@ -3,65 +3,63 @@ import random
 import string
 import supervisely_lib as sly
 
+TEAM_ID = int(os.environ['context.teamId'])
+WORKSPACE_ID = int(os.environ['context.workspaceId'])
+PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
+
 my_app = sly.AppService()
+PROJECT = None
+TOTAL_IMAGES_COUNT = None
 
-LENGTH = 5  # int(os.environ['modal.state.length'])
-
-
-@my_app.callback("generate")
+@my_app.callback("assign_tags")
 @sly.timeit
-def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
-    rand_string = ''.join((random.choice(string.ascii_letters + string.digits)) for _ in range(LENGTH))
-    rand_string = state["prefix"] + rand_string
-    api.task.set_field(task_id, "data.randomString", rand_string)
-
-
-@my_app.callback("preprocessing")
-@sly.timeit
-def preprocessing(api: sly.Api, task_id, context, state, app_logger):
-    sly.logger.info("do something here")
+def assign_tags(api: sly.Api, task_id, context, state, app_logger):
+    train_count = state["count.train"]
+    val_count = state["count.val"]
+    share_images = state["shareImagesBetweenSplits"]
+    if share_images is True:
+        if train_count != val_count:
+            raise ValueError("Share images option is enabled, but train_count != val_count")
+    else:
+        if train_count + val_count != TOTAL_IMAGES_COUNT:
+            raise ValueError("train_count + val_count != TOTAL_IMAGES_COUNT")
 
 
 def main():
-    sly.logger.info("Script arguments from modal dialog box", extra={"length: ": LENGTH})
+    sly.logger.info("Input params", extra={"context.teamId": TEAM_ID,
+                                           "context.workspaceId": WORKSPACE_ID,
+                                           "modal.state.slyProjectId": PROJECT_ID})
+
 
     api = sly.Api.from_env()
+    PROJECT = api.project.get_info_by_id(PROJECT_ID)
 
-    total_images_count = 850
+    TOTAL_IMAGES_COUNT = api.project.get_images_count(PROJECT.id)
     data = {
-        "projectId": 0,
-        "projectName": "0",
-        "projectPreviewUrl": "",
+        "projectId": PROJECT.id,
+        "projectName": PROJECT.name,
+        "projectPreviewUrl": api.image.preview_url(PROJECT.reference_image_url, 100, 100),
         "progress": 0,
-        "resultProjectId": 0,
+        "resultProjectId": None,
         "resultProject": "1",
         "resultProjectPreviewUrl": "",
         "started": False,
         "finished": False,
-        "totalImagesCount": total_images_count,
+        "totalImagesCount": TOTAL_IMAGES_COUNT,
         "table": [
-            {
-                "name": "train",
-                "type": "success"
-            },
-            {
-                "name": "val",
-                "type": "warning"
-            },
-            {
-                "name": "total",
-                "type": "gray",
-            },
+            {"name": "train", "type": "success"},
+            {"name": "val", "type": "warning"},
+            {"name": "total", "type": "gray"},
         ]
     }
 
     train_percent = 80
-    train_count = int(total_images_count / 100 * train_percent)
+    train_count = int(TOTAL_IMAGES_COUNT / 100 * train_percent)
     state = {
         "count": {
-            "total": total_images_count,
+            "total": TOTAL_IMAGES_COUNT,
             "train": train_count,
-            "val": total_images_count - train_count
+            "val": TOTAL_IMAGES_COUNT - train_count
         },
         "percent": {
             "total": 100,
@@ -72,16 +70,8 @@ def main():
         "sliderDisabled": False
     }
 
-    initial_events = [
-        {
-            "state": None,
-            "context": None,
-            "command": "preprocessing",
-        }
-    ]
-
     # Run application service
-    my_app.run(data=data, state=state, initial_events=initial_events)
+    my_app.run(data=data, state=state)
 
 
 if __name__ == "__main__":
