c0bd5dee5dbc30fbce1e22e43e37b56354d40d04
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-09-19 01:16:57 -0300

    Implement project copying and tagging functionality in assign_tags

--- src/tag_train_val_test.py
+++ src/tag_train_val_test.py
@@ -1,6 +1,7 @@
 import os
 import random
 from collections import defaultdict
+from typing import Dict, List, Optional, Set
 import supervisely as sly
 from supervisely.app.v1.app_service import AppService
 import workflow as w
@@ -47,6 +48,147 @@ def sample_images(api, datasets, train_images_count):
 
     return ds_images_train, ds_images_val, len(train_images), len(val_images)
 
+def _find_parents_in_tree(
+    tree: Dict[sly.DatasetInfo, Dict], dataset_id: int, with_self: bool = False
+) -> Optional[List[sly.DatasetInfo]]:
+    """
+    Find all parent datasets in the tree for a given dataset ID.
+    """
+
+    def _dfs(subtree: Dict[sly.DatasetInfo, Dict], parents: List[sly.DatasetInfo]):
+        for dataset_info, children in subtree.items():
+            if dataset_info.id == dataset_id:
+                if with_self:
+                    return parents + [dataset_info]
+                return parents
+            res = _dfs(children, parents + [dataset_info])
+            if res is not None:
+                return res
+        return None
+
+    return _dfs(tree, [])
+
+
+def copy_project(
+    api: sly.Api,
+    project_name: str,
+    workspace_id: int,
+    project_id: int,
+    dataset_ids: List[int] = [],
+    with_annotations: bool = True,
+):
+    """
+    Copy a project
+
+    :param api: Supervisely API
+    :type api: Api
+    :param project_name: Name of the new project
+    :type project_name: str
+    :param workspace_id: ID of the workspace
+    :type workspace_id: int
+    :param project_id: ID of the project to copy
+    :type project_id: int
+    :param dataset_ids: List of dataset IDs to copy. If empty, all datasets from the project will be copied.
+    :type dataset_ids: List[int]
+    :param with_annotations: Whether to copy annotations
+    :type with_annotations: bool
+    :param progress: Progress callback
+    :type progress: Progress
+    :return: Created project
+    :rtype: ProjectInfo
+    """
+
+    def _create_project() -> sly.ProjectInfo:
+        created_project = api.project.create(
+            workspace_id,
+            project_name,
+            type=sly.ProjectType.IMAGES,
+            change_name_if_conflict=True,
+        )
+        if with_annotations:
+            api.project.merge_metas(src_project_id=project_id, dst_project_id=created_project.id)
+        return created_project
+
+    def _copy_full_project(
+        created_project: sly.ProjectInfo, src_datasets_tree: Dict[sly.DatasetInfo, Dict]
+    ):
+        src_dst_ds_id_map: Dict[int, int] = {}
+
+        def _create_full_tree(ds_tree: Dict[sly.DatasetInfo, Dict], parent_id: int = None):
+            for src_ds, nested_src_ds_tree in ds_tree.items():
+                dst_ds = api.dataset.create(
+                    project_id=created_project.id,
+                    name=src_ds.name,
+                    description=src_ds.description,
+                    change_name_if_conflict=True,
+                    parent_id=parent_id,
+                )
+                src_dst_ds_id_map[src_ds.id] = dst_ds
+
+                # Preserve dataset custom data
+                info_ds = api.dataset.get_info_by_id(src_ds.id)
+                if info_ds.custom_data:
+                    api.dataset.update_custom_data(dst_ds.id, info_ds.custom_data)
+                _create_full_tree(nested_src_ds_tree, parent_id=dst_ds.id)
+
+        _create_full_tree(src_datasets_tree)
+
+        for src_ds_id, dst_ds in src_dst_ds_id_map.items():
+            _copy_items(src_ds_id, dst_ds)
+
+    def _copy_datasets(created_project: sly.ProjectInfo, src_datasets_tree: Dict[sly.DatasetInfo, Dict]):
+        created_datasets: Dict[int, sly.DatasetInfo] = {}
+        processed_copy: Set[int] = set()
+
+        for dataset_id in dataset_ids:
+            chain = _find_parents_in_tree(src_datasets_tree, dataset_id, with_self=True)
+            if not chain:
+                sly.logger.warning(
+                    f"Dataset id {dataset_id} not found in project {project_id}. Skipping."
+                )
+                continue
+
+            parent_created_id = None
+            for ds_info in chain:
+                if ds_info.id in created_datasets:
+                    parent_created_id = created_datasets[ds_info.id].id
+                    continue
+
+                created_ds = api.dataset.create(
+                    created_project.id,
+                    ds_info.name,
+                    description=ds_info.description,
+                    change_name_if_conflict=False,
+                    parent_id=parent_created_id,
+                )
+                created_datasets[ds_info.id] = created_ds
+                src_info = api.dataset.get_info_by_id(ds_info.id)
+                if src_info.custom_data:
+                    api.dataset.update_custom_data(created_ds.id, src_info.custom_data)
+                parent_created_id = created_ds.id
+
+            if dataset_id not in processed_copy:
+                _copy_items(dataset_id, created_datasets[dataset_id])
+                processed_copy.add(dataset_id)
+
+    def _copy_items(src_ds_id: int, dst_ds: sly.DatasetInfo):
+        input_img_infos = api.image.get_list(src_ds_id)
+        api.image.copy_batch_optimized(
+            src_dataset_id=src_ds_id,
+            src_image_infos=input_img_infos,
+            dst_dataset_id=dst_ds.id,
+            with_annotations=with_annotations,
+        )
+
+    created_project = _create_project()
+    src_datasets_tree = api.dataset.get_tree(project_id)
+
+    if not dataset_ids:
+        _copy_full_project(created_project, src_datasets_tree)
+    else:
+        _copy_datasets(created_project, src_datasets_tree)
+    return created_project
+
 def _assign_tag(task_id, api: sly.Api, split, tag_metas, new_project, created_datasets, progress):
     for dataset_id, images in split.items():
         dataset = api.dataset.get_info_by_id(dataset_id)
@@ -85,6 +227,35 @@ def _assign_tag(task_id, api: sly.Api, split, tag_metas, new_project, created_da
             ]
             api.task.set_fields(task_id, fields)
 
+def _assign_tag_inplace(task_id, api: sly.Api, split, tag_metas, progress):
+    for dataset_id, images in split.items():
+        dataset = api.dataset.get_info_by_id(dataset_id)
+        for batch in sly.batched(images):
+            image_ids = [image_info.id for image_info in batch]
+            ann_infos = api.annotation.download_batch(dataset.id, image_ids)
+            new_annotations = []
+
+            for ann_info in ann_infos:
+                ann_json = ann_info.annotation
+                new_ann = sly.Annotation.from_json(ann_json, META_ORIGINAL)
+                img_tags = new_ann.img_tags.items()
+                img_tags = [tag for tag in img_tags if tag.meta.name not in [TRAIN_NAME, VAL_NAME]]
+                for tag_meta in tag_metas:
+                    img_tags.append(sly.Tag(tag_meta))
+                new_ann = new_ann.clone(img_tags=sly.TagCollection(img_tags))
+                new_annotations.append(new_ann)
+
+            api.annotation.upload_anns(image_ids, new_annotations)
+
+            progress.iters_done_report(len(batch))
+            progress_percent = int(progress.current * 100 / progress.total)
+            fields = [
+                {"field": "data.progressCurrent", "payload": progress.current},
+                {"field": "data.progressTotal", "payload": progress.total},
+                {"field": "data.progress", "payload": progress_percent},
+            ]
+            api.task.set_fields(task_id, fields)
+
 def validate_project_name(name):
     if not name or name.strip() == "":
         return f"{PROJECT.name}_tagged"
@@ -108,29 +279,25 @@ def assign_tags(api: sly.Api, task_id, context, state, app_logger):
     if inplace is True:
         raise NotImplementedError("Inplace operation will be implemented in the future...")
 
-    datasets = api.dataset.get_list(PROJECT.id, recursive=True)
-    images_train, images_val, _cnt_train, _cnt_val = sample_images(api, datasets, train_count)
-
     res_name = validate_project_name(state["resultProjectName"])
-    new_project = api.project.create(WORKSPACE_ID, res_name, sly.ProjectType.IMAGES,
-                                     description="train/val", change_name_if_conflict=True)
-    api.project.update_meta(new_project.id, META_RESULT.to_json())
+    new_project = copy_project(api, res_name, WORKSPACE_ID, PROJECT.id)
+    
+    datasets = api.dataset.get_list(new_project.id, recursive=True)
+    images_train, images_val, _cnt_train, _cnt_val = sample_images(api, datasets, train_count)
 
     progress = sly.Progress("Tagging", TOTAL_IMAGES_COUNT)
 
     if share_images is True:
         if _cnt_val == 0:
-            _created_datasets = {}
-            _assign_tag(task_id, api, images_train, [TRAIN_TAG_META, VAL_TAG_META], new_project, _created_datasets, progress)
+            _assign_tag_inplace(task_id, api, images_train, [TRAIN_TAG_META, VAL_TAG_META], progress)
         else:
             raise RuntimeError("_cnt_val != 0")
     else:
         if train_count + val_count != TOTAL_IMAGES_COUNT:
             raise ValueError("train_count + val_count != TOTAL_IMAGES_COUNT")
 
-        _created_datasets = {}
-        _assign_tag(task_id, api, images_train, [TRAIN_TAG_META], new_project, _created_datasets, progress)
-        _assign_tag(task_id, api, images_val, [VAL_TAG_META], new_project, _created_datasets, progress)
+        _assign_tag_inplace(task_id, api, images_train, [TRAIN_TAG_META], progress)
+        _assign_tag_inplace(task_id, api, images_val, [VAL_TAG_META], progress)
 
     # to get correct "reference_image_url"
     new_project = api.project.get_info_by_id(new_project.id)
